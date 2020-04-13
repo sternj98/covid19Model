64d3ebb9d742c166a0e72489db284a3be3eab75e
sternj98 <sternj@college.harvard.edu>
2020-04-13 01:21:47 -0400

    minimal viable product

--- PopulationClasses.py
+++ PopulationClasses.py
@@ -1,5 +1,8 @@
 import numpy as np
 from numpy import random as rnd
+import random
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 # defining constant integers to make labeling easy
 SUS = 0 # susceptible
@@ -7,9 +10,12 @@ INF = 1 # infected
 REC = 2 # recovered
 DEAD = 3 # dead
 
+incubation = 5 # 5 days of infecting people, then isolate
+
 # vector of deathrates for age categories;
 # data from https://www.cdc.gov/mmwr/volumes/69/wr/mm6912e2.htm#F2_down
 # age categories: 0-19,20-44,45-54,55-65,65-74,75-84,>85
+
 deathrate = {0: 0,
              20:.0015,
              45: .0065,
@@ -18,36 +24,103 @@ deathrate = {0: 0,
              75: .075,
              85: .15}
 
-p_infect = .9999 # get this from diamond cruise ship
+age_keys = list(deathrate.keys())
 
 class Person:
-    def __init__(self,age):
+    def __init__(self,age,id):
+        self.id = id
         self.age = age # dictionary key in {0,20,45,55,65,75,85}
         self.dr = deathrate[age]
         self.status = SUS # start as healthy susceptible
-
-    # these connections are constant, other connections are randomly chosed every day
-    def defineFamily(self,connVector):
         self.connections = []
-        for neuronIndex in range(len(outputWeightVector)):
-            if outputWeightVector[neuronIndex] > 0:
-                self.outputs.append([neuronIndex,outputWeightVector[neuronIndex],outputDelayVector[neuronIndex]])
+
+    # Define connected individuals to potentially infect later
+    def defineConnections(self,connVector):
+        for citizen in range(len(connVector)):
+            if connVector[citizen] > 0:
+                self.connections.append(citizen) # this is a person we may infect
 
     def getInfected(self):
         self.status = INF
         self.infection_counter = 0 # days sick
-        # query mortality:
-        if rnd.rand() < deathrate[self.age]:
-            self.fatal = True
-            self.days_to_death = round(rnd.normal(25,3)) # people stay in the hospital for a long time
+        # query mortality: (incr deathrate now for testing)
+        if rnd.rand() <  deathrate[self.age]:
+            self.fatal = True   # should be 21
+            self.days_to_death = round(rnd.normal(10,3)) # people stay in the hospital for a long time
+        else:
+            self.fatal = False
+            self.days_to_recovery = round(rnd.normal(10,7)) # probably around 3 weeks with a large std
+
+    def step(self):
+        self.infection_counter += 1 # add a day to the infection counter
+        if self.fatal == True:
+            if self.infection_counter == self.days_to_death:
+                self.status = DEAD
+        else:
+            if self.infection_counter == self.days_to_recovery:
+                self.status = REC
+        if self.infection_counter > incubation:
+            self.connections = [] # quarantine; don't come out because don't care about recovered
+
+        return self.status
 
 class Population:
-    def __init__(self,age_vector,I0):
-        self.size = np.sum(age_vector)
-        self.nSus = self.size
-        self.nInf = I0 # initial number of infected in population
-        self.nRec = 0
-        self.nDead = 0
+    def __init__(self,size,age_dist,I0,p_connect):
+        self.size = size
         self.connections = np.zeros((self.size,self.size))
+        self.I0 = I0
 
-    def defineFamilyConnections(self):
+        self.statuses = np.zeros(self.size) # vector of statuses for visualization
+
+        self.infectedPool = [] # initialize empty list that will be populated with infected
+
+        self.people = [] # Define population in a list
+
+        id = 0
+        for agegroup in range(len(age_dist)):
+            for citizens in range(int(size * age_dist[agegroup])):
+                self.people.append(Person(age_keys[agegroup],id))
+                id += 1
+        while len(self.people) < size:
+            self.people.append(Person(age_keys[0],id)) # round it off with some babies
+            id += 1
+        # sparse, (currently) uniform, and static, connectivity matrix
+        self.C = rnd.binomial(1,p_connect,size ** 2).reshape((size,size))
+        np.fill_diagonal(self.C,0) # can't infect yourself you wanker
+
+        for citizen in range(size):
+            self.people[citizen].defineConnections(self.C[citizen,:])
+
+        # infect initial people
+        for i in range(I0):
+            idx = random.choice(list(range(size)))
+            self.people[idx].getInfected()
+            self.statuses[i] = INF
+            self.infectedPool.append(self.people[idx])
+
+    def prepSimulation(self,nDays):
+        """
+            Initialize population to prepare for simulation of nDays
+        """
+        self.nSus = np.zeros(nDays+1)
+        self.nSus[0] = self.size # initial susceptible
+        self.nInf = np.zeros(nDays+1)
+        self.nInf[0] = self.I0 # initial number of infected in population
+        self.nRec = np.zeros(nDays+1)
+        self.nRec[0] = 0
+        self.nDead = np.zeros(nDays+1)
+        self.nDead[0] = 0
+
+    def showConnections(self):
+        plt.figure()
+        sns.heatmap(self.C,cmap='hot')
+        plt.suptitle('Population Connectivity')
+
+    def plotStatistics(self):
+        plt.figure()
+        plt.plot(self.nSus,label = "Susceptible")
+        plt.plot(self.nInf,label = "Infected")
+        plt.plot(self.nRec,label = "Recovered")
+        plt.plot(self.nDead,label = "Dead")
+        plt.legend()
+        plt.title('Population Statistics over Time')
---
+++ runSimulation.py
@@ -0,0 +1,79 @@
+import numpy as np
+from numpy import random as rnd
+import random
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+from PopulationClasses import Population
+
+# defining constant integers to make labeling easy
+SUS = 0 # susceptible
+INF = 1 # infected
+REC = 2 # recovered
+DEAD = 3 # dead
+
+age_dist= np.array([6.0, 6.1, 6.5, 6.6, 6.6, 7.1, 6.7, 6.6, 6.1, 6.3, 6.4, 6.6, 6.3, 5.2, 4.1, 2.9, 1.9, 1.9])
+under20 = np.sum(age_dist[0:4])
+twentyto45 = np.sum(age_dist[4:9])
+forty5to55 = np.sum(age_dist[9:11])
+fifty5to65 = np.sum(age_dist[11:13])
+sixty5to75 = np.sum(age_dist[13:15])
+seventy5to85 = np.sum(age_dist[15:17])
+eighty5plus = np.sum(age_dist[17:len(age_dist)])
+
+#arranged ages according to the intervals set for death rates
+age_dist = np.array([under20,twentyto45,forty5to55,fifty5to65,sixty5to75,seventy5to85,eighty5plus])
+age_dist = age_dist / np.sum(age_dist) # probability distribution over ages
+
+# create a population under some parameters and run a simulation of infection over time
+# note that one timestep is a day
+nDays = 30
+size = 10000
+I0 = 5
+p_connect = 10 / size # have connections with avg of 10 people
+
+population = Population(size,age_dist,I0,p_connect)
+print("Done with initialization")
+population.prepSimulation(nDays)
+
+p_infect = .05 # get this from diamond cruise ship
+
+# population.showConnections()
+# plt.show() # this just takes a lot of time if you don't
+
+for day in range(1,nDays+1): # count 0 as initial day
+    # 0. modulate connectivity based on how much social distancing is happening
+        # random.choice([all indices]) #
+
+    # 1. propagate infection and advance infections
+    nInfected = len(population.infectedPool)
+    infectCount = 0
+
+    for infected in population.infectedPool:
+        infectCount += 1
+        new_status = infected.step()
+        if new_status != INF: # change status
+            population.infectedPool.remove(infected) # take out of infected pool
+            population.statuses[infected.id] = new_status
+        else: # we're still infected
+            for conn_idx in infected.connections:
+                if population.people[conn_idx].status == SUS:
+                    if rnd.rand() < p_infect:
+                        population.people[conn_idx].getInfected()
+                        population.infectedPool.append(population.people[conn_idx])
+                        population.statuses[conn_idx] = INF
+        if infectCount == nInfected: # to prevent infinite infection recursion
+            break
+
+    # 2. record the new population statistics
+    population.nSus[day] = len(np.where(population.statuses == SUS)[0])
+    population.nRec[day] = len(np.where(population.statuses == REC)[0])
+    population.nInf[day] = len(np.where(population.statuses == INF)[0])
+    population.nDead[day] = len(np.where(population.statuses == DEAD)[0])
+
+plt.figure()
+print(population.nDead[-1])
+sns.heatmap(population.statuses.reshape((100,100)))
+population.plotStatistics()
+plt.show()
